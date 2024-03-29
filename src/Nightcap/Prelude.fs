25147aa778caf079347ed1c4dfc57c4e992781ef
valor <valorzhong@gmail.com>
2024-03-29 14:13:01 +0800

    New method

--- src/Nightcap/Prelude.fs
+++ src/Nightcap/Prelude.fs
@@ -9,10 +9,12 @@ module Prelude =
             failwith message
 
     let orJust = Option.defaultValue
+    
+    let orWith = Option.defaultWith
 
     module Operators =
 
-        let (|?) = defaultArg
+        let (|?) a b = defaultArg b a
 
         let inline (!!) (x: ^a) : ^b =
             ((^a or ^b): (static member op_Implicit: ^a -> ^b) x)
